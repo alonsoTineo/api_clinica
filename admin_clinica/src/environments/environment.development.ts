00960bfdb7a5b46510ea6eb3c01c279fa5f3cf67
Kevin Alonso <alonsotk@gmail.com>
2024-06-12 22:28:29 -0500

    se craó el login por usuario con token tanto el back como front

---
+++ admin_clinica/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// aqui van las variables de entorno de desarrollo
+export const environment = {
+    URL_BACKEND:'http://127.0.0.1:8000/',
+    URL_SERVICIOS:"http://127.0.0.1:8000/api",
+    URL_FRONTEND:"http://localhost:4200/"
+};
